ad8eaa35aa210e79c64892f68df34b9eb72f0eea
Ruchir Kalokhe <ruchirkalokhe@gmail.com>
2026-02-05 12:53:16 +0530

    refactor: add Vercel analytics injection to main entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { inject } from "@vercel/analytics";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+
+inject();
